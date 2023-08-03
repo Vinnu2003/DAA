1409d58f8120f685db806c29b96cd7202b53c1a3
Vinnu2003 <114645132+Vinnu2003@users.noreply.github.com>
2023-08-03 08:35:19 +0530

    OUTPUT
    Enter a string: 121
    The string is a palindrome.

---
+++ Palindrome.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+bool isPalindrome(const char *str) {
+    int left = 0;
+    int right = strlen(str) - 1;
+    while (left < right) {
+        if (str[left] != str[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    if (isPalindrome(str)) {
+        printf("The string is a palindrome.\n");
+    } else {
+        printf("The string is not a palindrome.\n");
+    }
+
+    return 0;
+}
